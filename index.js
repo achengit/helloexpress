247ce0d6a821018f77a9c8b3d74ec22f36070256
Aaron Chen <v-aach@microsoft.com>
2017-09-18 16:38:20 +0800

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Azure!')
+  res.send('Hello Express!')
 })
 
 app.listen(3000, function () {
